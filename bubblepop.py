995c640e82a157e395b337174b74dfc396899b46
Dan Lazewatsky <dan@lazewatsky.com>
2014-10-03 14:15:24 -0500

    add command line args, and write data to a file

--- bubblepop.py
+++ bubblepop.py
@@ -346,12 +346,33 @@ class BubbleGame(object):
          
         pygame.quit()
 
-from math import hypot
-def gather_data(*args):
-    d = hypot(*np.array(args[0])-args[1])
-    print d, args[5], args[2]
-
 if __name__ == '__main__':
-    game = BubbleGame(bubble_sizes=(20,100), fullscreen=False)
+    import argparse, sys, datetime, os, csv
+    parser = argparse.ArgumentParser(description='Bubble Pop game.')
+    parser.add_argument('--minsize', type=int, default=20,  help="Minimum bubble size (in pixels)")
+    parser.add_argument('--maxsize', type=int, default=100, help="Maximum bubble size (in pixels)")
+    parser.add_argument('-f', '--fullscreen', default=False, action='store_true')
+    parser.add_argument('-o', '--output-prefix', type=str, default='', metavar='PREFIX', help='Prepend PREFIX to file name')
+    args = parser.parse_args()
+    if args.minsize >= args.maxsize:
+        print >> sys.stderr, 'minsize (%s) must be smaller than maxsize (%s)'  % (args.minsize, args.maxsize)
+        sys.exit(1)
+
+    game = BubbleGame(bubble_sizes=(args.minsize,args.maxsize), fullscreen=args.fullscreen)
+
+    timestr = datetime.datetime.today().strftime('%d-%m-%y-%H.%M.%f')
+    filename = '%s_%s.csv' % (args.output_prefix, timestr)
+    csvfile = open(filename, 'w')
+    writer = csv.writer(csvfile)
+    
+    print 'Writing data to', filename
+    from math import hypot
+    def gather_data(*args):
+        d = hypot(*np.array(args[0])-args[1])
+        print d, args[5], args[2]
+        writer.writerow([d, args[5], args[2]])
+
     game.movement.connect(gather_data)
+
     game.run()
+    csvfile.close()
